3553173a0a157aaa3c265b093ff9418bece8be7a
Frank Santos <franksantosfung@gmail.com>
2024-04-30 20:38:21 -0400

    Updated List to sort better

--- public_html/Project/admin/list_teams.php
+++ public_html/Project/admin/list_teams.php
@@ -9,19 +9,12 @@ if (is_logged_in(true)) {
 // build search form
 $form = [
     ["type" => "text", "name" => "name", "placeholder" => "Team Name", "label" => "Team Name", "include_margin" => false],
-
-   // ["type" => "text", "name" => "coach", "placeholder" => "Coach", "label" => "Coach", "include_margin" => false],
-
-   // ["type" => "text", "name" => "league", "placeholder" => "League", "label" => "League", "include_margin" => false],
-
     //["type" => "select", "name" => "sort", "label" => "Sort", "options" => ["team_name" => "Name", "coach" => "Coach", "founded" => "Founded"], "include_margin" => false],
     //["type" => "select", "name" => "order", "label" => "Order", "options" => ["asc" => "Ascending", "desc" => "Descending"], "include_margin" => false],
-
     ["type" => "number", "name" => "limit", "label" => "Limit", "value" => "10", "include_margin" => false],
 ];
 
 $query = "SELECT id, Team_Name FROM Teams WHERE 1=1";
-//$query = "SELECT id, league, team_name, coach FROM Teams WHERE 1=1";
 $params = [];
 
 // Add conditions based on form input
@@ -30,20 +23,15 @@ if (!empty($_GET["name"])) {
     $params[":name"] = "%" . $_GET["name"] . "%";
 }
 
-if (!empty($_GET["coach"])) {
-    $query .= " AND coach LIKE :coach";
-    $params[":coach"] = "%" . $_GET["coach"] . "%";
-}
-
-if (!empty($_GET["league"])) {
-    $query .= " AND league LIKE :league";
-    $params[":league"] = "%" . $_GET["league"] . "%";
-}
-
 // Sort and order
 $sort = isset($_GET["sort"]) ? $_GET["sort"] : "team_name";
 $order = isset($_GET["order"]) ? $_GET["order"] : "asc";
-$query .= " ORDER BY $sort $order";
+
+if (isset($_GET["sort_alphabet"])) {
+    $query .= " ORDER BY team_name ASC";
+} else {
+    $query .= " ORDER BY $sort $order";
+}
 
 // Limit
 $limit = isset($_GET["limit"]) ? $_GET["limit"] : 10;
@@ -81,13 +69,15 @@ $table = [
                     <?php render_input($v); ?>
                 </div>
             <?php endforeach; ?>
+            <div class="col">
+                <?php render_button(["text" => "Search", "type" => "submit", "text" => "Filter"]); ?>
+                <?php render_button(["text" => "Sort Alphabetically", "type" => "submit", "name" => "sort_alphabet"]); ?>
+                <a href="?clear" class="btn btn-secondary">Clear</a>
+            </div>
         </div>
-        <?php render_button(["text" => "Search", "type" => "submit", "text" => "Filter"]); ?>
-        <a href="?clear" class="btn btn-secondary">Clear</a>
     </form>
     <?php render_table($table); ?>
 </div>
 <?php
-//note we need to go up 1 more directory
 require_once(__DIR__ . "/../../../partials/flash.php");
 ?>
